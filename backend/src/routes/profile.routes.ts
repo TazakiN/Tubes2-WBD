668c8e9d23c8e85c9875abd5ea87c03c11c00b21
TazakiN <tazkia.nizami@gmail.com>
2024-11-23 10:48:13 +0700

    refactor: move jwt verification to middlewares

--- backend/src/routes/profile.routes.ts
+++ backend/src/routes/profile.routes.ts
@@ -1,19 +1,11 @@
-import { Context, Hono, Next } from "hono";
+import { Hono } from "hono";
 import { profileController } from "../controllers/profile.controller";
-import { jwt } from "hono/jwt";
 import { JwtVariables } from "hono/jwt";
+import verifyJWT from "../middlewares/verifyJWT";
 
 export const profileRoutes = new Hono<{ Variables: JwtVariables }>();
 
-profileRoutes.use("*", (c: Context, next: Next) => {
-  const secret = process.env.JWT_SECRET!;
-
-  const jwtMiddleware = jwt({
-    secret,
-    cookie: "token",
-  });
-  return jwtMiddleware(c, next);
-});
+profileRoutes.use("*", verifyJWT);
 
 profileRoutes.get("/:user_id", async (c) => {
   return await profileController.getProfile(c);
