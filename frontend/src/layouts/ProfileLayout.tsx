6a9213d4215191f36094a391e9ff6aedac773f24
Farhannr28 <farhannafis281004@gmail.com>
2024-11-26 19:10:46 +0700

    feat: Profile Layout (unfinished)

--- frontend/src/layouts/ProfileLayout.tsx
+++ frontend/src/layouts/ProfileLayout.tsx
@@ -1,26 +1,24 @@
 import { ReactNode } from "react";
-import linkedInLogo from "@/assets/linkedin.svg";
+import default_profile_picture from '@/assets/default-profile-picture.jpg'
 
 interface ProfileLayoutProps {
+  name: string;
+  profile_picture: string;
+  num_connection: number;
   children: ReactNode;
-  title: string;
+
 }
 
-export function ProfileLayout({ children, title }: ProfileLayoutProps) {
+export function ProfileLayout({ name = "Olajide Olayinka Williams", profile_picture = default_profile_picture, num_connection = 0, children }: ProfileLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+    <div className="min-h-screen flex justify-center bg-gray-light">
+      <div className="flex flex-col justify-center py-12 sm:px-24 max-w-screen-lg w-full mx-auto">
+        <div className="bg-blue-secondary h-24 rounded-t-lg"></div>
         <img
-          className="mx-auto h-20 w-auto"
-          src={linkedInLogo}
-          alt="Linkinpurry"
+          className="mx-auto h-32 w-auto rounded-full border-4 border-white"
+          src={profile_picture}
+          alt={"Profile picture of " + name}
         />
-        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-          {title}
-        </h2>
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
           {children}
         </div>
