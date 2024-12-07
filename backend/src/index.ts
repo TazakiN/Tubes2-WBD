96c181a21844a5905046e8d0d22a601bbe748509
TazakiN <tazkia.nizami@gmail.com>
2024-12-07 13:57:16 +0700

    fix: update CORS policy to allow all origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ app.use(logger());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     allowMethods: ["GET", "POST", "PUT", "DELETE"],
     allowHeaders: ["Content-Type", "Authorization"],
     maxAge: 6000,
