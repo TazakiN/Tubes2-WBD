f4aeb3503ea1bfe327cdba9dcc6f7ea6bfe6f1fb
TazakiN <tazkia.nizami@gmail.com>
2024-12-07 22:11:06 +0700

    feat: implement caching for feed and profile routes to improve performance

--- backend/src/routes/feed/feed.index.ts
+++ backend/src/routes/feed/feed.index.ts
@@ -2,10 +2,17 @@ import { OpenAPIHono } from "@hono/zod-openapi";
 
 import * as handlers from "./feed.handlers";
 import * as routes from "./feed.routes";
+import { cache } from "hono/cache";
 
-const feedsRouter = new OpenAPIHono().openapi(
-  routes.getAllFeeds,
-  handlers.getAllFeeds
-);
+const feedsRouter = new OpenAPIHono().get(
+  "*",
+  cache({
+    cacheName: "my-cache",
+    wait: true,
+    cacheControl: "max-age=3600",
+    keyGenerator: (c) => c.req.url,
+  })
+) as OpenAPIHono;
+feedsRouter.openapi(routes.getAllFeeds, handlers.getAllFeeds);
 
 export default feedsRouter;
--- backend/src/routes/profile/profile.index.ts
+++ backend/src/routes/profile/profile.index.ts
@@ -2,8 +2,19 @@ import { OpenAPIHono } from "@hono/zod-openapi";
 
 import * as handlers from "./profile.handlers";
 import * as routes from "./profile.routes";
+import { cache } from "hono/cache";
 
-const profileRouter = new OpenAPIHono()
+const profileRouter = new OpenAPIHono().get(
+  "*",
+  cache({
+    cacheName: "my-cache",
+    wait: true,
+    cacheControl: "max-age=3600",
+    keyGenerator: (c) => c.req.url,
+  })
+) as OpenAPIHono;
+
+profileRouter
   .openapi(routes.getProfileInfo, handlers.getProfileInfo)
   .openapi(routes.getProfile, handlers.getProfile);
 
