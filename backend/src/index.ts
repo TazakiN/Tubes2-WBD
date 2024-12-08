983dc6d0407323d8e214685330142142b7a1daf4
TazakiN <tazkia.nizami@gmail.com>
2024-12-08 07:03:40 +0700

    fix: idk

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,8 +33,8 @@ app.use(logger());
 app.use(
   rateLimiter({
     windowMs: 3 * 1000, // 10 seconds
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    limit: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     keyGenerator: (c) => "<unique_key>", // Method to generate custom identifiers for clients.
     // store: "memory", // Store the rate limit data in memory.
   })
