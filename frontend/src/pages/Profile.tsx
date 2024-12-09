3a7f88a2f3a321f40284e99bb75c97aedd65b2be
TazakiN <tazkia.nizami@gmail.com>
2024-12-09 22:36:34 +0700

    feat: add user_id prop to ProfileLayout and ProfileConditionalButton components

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ProfileLayout } from "@/layouts/ProfileLayout";
 import { ProfileData } from "@/lib/types/userData";
 import { toast } from "sonner";
@@ -15,25 +15,26 @@ const Profile = () => {
   const [authenticated, setAuthenticated] = useState(false);
   const [owner, setOwner] = useState(false);
   const [profileData, setProfileData] = useState<ProfileData>({});
+  const user_id = useRef<string | null>(null);
   useEffect(() => {
     const fetchProfile = async () => {
       try {
         setLoading(true);
-        let user_id = getUserIdFromUrl();
+        user_id.current = getUserIdFromUrl();
 
-        if (user_id == "profile") {
+        if (user_id.current == "profile") {
           const response = await fetch(`${BASE_URL}/profile/info`, {
             credentials: "include",
           });
           if (!response.ok) throw new Error("User not found");
           const data = await response.json();
           setAuthenticated(true);
-          user_id = `/${data.data.id}`;
+          user_id.current = `/${data.data.id}`;
         } else {
-          user_id = `/${user_id}`;
+          user_id.current = `/${user_id.current}`;
         }
 
-        const requestPath = `${BASE_URL}/profile${user_id}`;
+        const requestPath = `${BASE_URL}/profile${user_id.current}`;
         const response = await fetch(requestPath, { credentials: "include" });
         if (!response.ok) throw new Error("User not found");
         const data = await response.json();
@@ -57,6 +58,7 @@ const Profile = () => {
 
   return (
     <ProfileLayout
+      user_id={user_id.current ?? ""}
       profile={profileData}
       authenticated={authenticated}
       owner={owner}
